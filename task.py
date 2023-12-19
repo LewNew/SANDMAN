a14bee5672d9c107914a49720fd2edff14028117
Hyland <hylandr@lancaster.ac.uk>
2023-12-19 12:29:17 +0000

    added basic task class and assigned with metadata and print methods

---
+++ task.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+class Task:
+    """
+    Represents a task with details such as name, type, inception time, percent complete, and last worked on.
+
+    Attributes:
+        name (str): The name of the task.
+        type (str): The type or category of the task.
+        percent_complete (int): The percentage of completion for the task.
+        last_worked_on (str, optional): The date when the task was last worked on. Default is None.
+        inception_time (str, optional): The date and time when the task was created. Default is the current time.
+
+    Methods:
+        display_task_details(): Display detailed information about the task.
+        set_last_worked_on(datatime=None): Set the last worked on time for the task. If no time is provided, the current time is used.
+    """
+
+    def __init__(self, name, task_type, percent_complete=0, last_worked_on=None, inception_time=None):
+        """
+        Initializes a new Task object.
+
+        Parameters:
+            name (str): The name of the task.
+            task_type (str): The type or category of the task.
+            percent_complete (int, optional): The percentage of completion for the task.
+            last_worked_on (str, optional): The date when the task was last worked on. Default is None.
+            inception_time (str, optional): The date and time when the task was created. Default is the current time.
+        """
+
+        self.name = name
+        self.task_type = task_type
+        self.percent_complete = percent_complete
+        #TODO add functionality to manulary set last_worked_on and inception_time
+        self.last_worked_on = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.inception_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+    def set_last_worked_on(self,datatime=None):
+         """
+        Set the last worked on time for the task. If no time is provided, the current time is used.
+
+        Parameters:
+            datatime (str, optional): The date and time to set as the last worked on time. Default is None.
+        """
+        #TODO add functionality to manulary set last_worked_on
+        self.last_worked_on = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def display_task_details(self):
+        """
+        Display detailed information about the task.
+        """
+        print(f"Task: {self.name}")
+        print(f"Type: {self.task_type}")
+        print(f"Inception Time: {self.inception_time}")
+        print(f"Percent Complete: {self.percent_complete}%")
+        print(f"Last Worked On: {self.last_worked_on}")
+
+    
+
+        
+if __name__ == "__main__":
+    # Example usage:
+    task1 = Task("Project A", "Development")
+    task1.display_task_details()
+    task1.set_last_worked_on()
