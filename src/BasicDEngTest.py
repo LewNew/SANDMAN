2b8f983b68ffa8905411e671e90a3c5f69079720
Hyland <hylandr@lancaster.ac.uk>
2023-12-22 13:22:05 +0000

    fleshed out basicDengTest to show add and remove functionality, added a bery basic planTaskTask that adds 2 tasks, however this is very hard coded and needs to be changed

--- src/BasicDEngTest.py
+++ src/BasicDEngTest.py
@@ -5,27 +5,35 @@ from Channel import Channel
 from NotepadChannel import NotepadChannel
 from RAWChannel import RAWChannel
 from TaskList import TaskList
+from PlanTaskTask import PlanTaskTask
 
 if __name__ == "__main__":
 
     taskList = TaskList()
 
-    print(taskList)
-
-    notepadTask1 = NotepadTask("q2Report","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework1.txt",task_list=taskList)
-    notepadTask2 = NotepadTask("scriptForPresentation","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework2.txt",task_list=taskList)
-    notepadTask3 = NotepadTask("resaerchPaper","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework3.txt",task_list=taskList)
-
-    taskList.add_task(notepadTask1)
-    # print(taskList)
-    notepadTask1.add_to_parent_task_list(notepadTask2)
-    print(taskList)
-    # notepadTask1.remvoe_from_parent_task_list(notepadTask2)
     # print(taskList)
 
+    # notepadTask1 = NotepadTask("q2Report","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework1.txt",task_list=taskList)
+    # notepadTask2 = NotepadTask("scriptForPresentation","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework2.txt",task_list=taskList)
+    # notepadTask3 = NotepadTask("resaerchPaper","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework3.txt",task_list=taskList)
+
+    # taskList.add_task(notepadTask1)
+    # # print(taskList)
+    # notepadTask1.add_to_parent_task_list(notepadTask2)
+    # print(taskList)
+    # # notepadTask1.remvoe_from_parent_task_list(notepadTask2)
+    # # print(taskList)
+
+    print(taskList)
+
+    taskList.add_task(PlanTaskTask("taskPlan","taskPlan",task_list=taskList))
+
+    print(taskList)
 
     for task in taskList:
         task.do_work(task=task,persona=None,mood=None,memory=None)
+
+    print(taskList)
 
     task_to_remove = []
     for task in taskList:
@@ -35,4 +43,4 @@ if __name__ == "__main__":
     for task in task_to_remove:
         taskList.remove_task(task)
 
-    
+    print(taskList)
