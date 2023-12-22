13500127ea2c1d6ee4f36f972e6a41d37bb5ef77
lewnew <https://lewnew@github.com>
2023-12-22 14:18:56 +0000

    Fixed generate_text AGAIN

--- src/TextGenerator.py
+++ src/TextGenerator.py
@@ -1,8 +1,6 @@
-import openai
-import requests
-from prompts import gpt_requests
-from Mood import Mood
+from openai import OpenAI
 
+client = OpenAI(api_key="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")
 
 class TextGenerator:
 
@@ -10,54 +8,42 @@ class TextGenerator:
         self.api_key = api_key
 
     def generate_text(self, task, persona, mood):
-        # Setting up the prompt
-        system_msg = f"You are a text generator. Your task is {task}. Your persona is {persona}, and your current mood is {mood}."
+        # Setting up the prompt to be more dynamic
+        system_msg = f"You are a {persona}, skilled in {task}. Your current mood is {mood}."
         messages = [
-            {"role": "system", "content": system_msg}
+            {"role": "system", "content": system_msg},
+            {"role": "user", "content": f"Please help me with {task}."}  # User's request can be more specific based on actual use case
         ]
 
         # Making the API call
-        openai.api_key = self.api_key
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # or the most appropriate model you have access to
-            messages=messages,
-        )
+        
+        response = client.chat.completions.create(model="gpt-3.5-turbo",  # or the most appropriate model you have access to
+        messages=messages)
 
         # Extracting and returning the generated text
         return response.choices[0].message.content
-
-    '''
-    def generate_text(self, task, persona, mood):
-        system_msg = f"You are {self.name}. "
-        user_prompt = f"Your task is {task}. Your persona is {persona}, and your current mood is {mood}."
-        return gpt_requests.ChatGPT_request(user_prompt, system_msg)
-    '''
 
     def generate_file_name(self, task, persona):
         base_name = f"{persona}_{task}".replace(" ", "_")
         return f"{base_name}.txt"
 
-#TODO should this not be in a if __name__ == "__main__" ??
-
 if __name__ == "__main__":
 
-    # Initialize the Mood object (assuming you've already done so)
-    agent_mood = Mood()
-    agent_mood.randomize_mood()
-
     # Define the task and persona
-    task_description = "Write a report"
-    persona_description = "Research Analyst"
+    task_description = "compose a poem"
+    persona_description = "poetic assistant"
+    mood_description = "creative"
 
-    # Retrieve the current mood of the agent
-    current_mood = agent_mood.get_mood()
+    # Initialize TextGenerator with an API key
+    text_generator = TextGenerator(api_key="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")
 
-    # Initialize and use the TextGenerator
-    api_key = "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"
-    text_generator = TextGenerator(api_key=api_key)
-    generated_text = text_generator.generate_text(task_description, persona_description, current_mood)
-
-    # Output the results
-    print(f"Mood: {current_mood}")
+    # Generate text
+    generated_text = text_generator.generate_text(task_description, persona_description, mood_description)
     print(f"Generated Text: {generated_text}")
 
+    # Assuming a mood is set here for the sake of the example
+    current_mood = "Happy"
+
+    # Print out the updated prompt from the program itself
+    system_msg = f"You are a text generator. Your task is {task_description}. Your persona is {persona_description}, and your current mood is {current_mood}."
+    print(f"System Message: {system_msg}")
