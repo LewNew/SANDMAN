1aa222dd86f6dd34d864f6ce9a2923ea4b0b0030
Hyland <hylandr@lancaster.ac.uk>
2023-12-22 13:23:26 +0000

    added comments

--- src/PlanTaskTask.py
+++ src/PlanTaskTask.py
@@ -5,7 +5,9 @@ from RAWChannel import RAWChannel
 from NotepadTask import NotepadTask
 
 class PlanTaskTask(Task):
-    
+    """
+    a task to add more tasks, currently very basic and must be changed
+    """
     def __init__(self, name, task_type, percent_complete=0, last_worked_on=None, inception_time=None,task_list=None):
 
 
@@ -18,6 +20,7 @@ class PlanTaskTask(Task):
     def do_work(self,task=None,persona=None,mood=None,memory=None):
         print("doing work")
 
+        #TODO very hard coded must be changed
         task.add_to_parent_task_list(NotepadTask("q2Report","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework1.txt",task_list=self.task_list))
         task.add_to_parent_task_list(NotepadTask("scriptForPresentation","typeing","H:\\PhD\\sandman\\project\\SANDMAN\\fakeWork\\","fakework2.txt",task_list=self.task_list))
 
