81772294d67515e7a36bf84ae3d9609eb457df42
Hyland <hylandr@lancaster.ac.uk>
2024-01-02 12:16:05 +0000

    changed some dumb code, do_work() no longer requires a task to be passed in this because do_work is in a task so you when do work needs task data it can just look at self

--- src/NotepadTask.py
+++ src/NotepadTask.py
@@ -23,14 +23,14 @@ class NotepadTask(Task):
         self.rawChannel = RAWChannel(self.file_path,self.file_name)
         
         
-    def do_work(self,task=None,persona=None,mood=None,memory=None):
+    def do_work(self,persona=None,mood=None,memory=None):
         print("doing work")
         #TODO  is currenetly hard coded, persoan and mood should be from what is passed into the do_work function
-        self.channel.send(text = self.generator.generate_text(task,persona,mood))
+        self.channel.send(text = self.generator.generate_text(self,persona,mood))
         print("finished work")
 
         #update task so that its finished
-        task.finish_work()
+        self.finish_work()
 
         #TODO do work should return some usefull value
         return True
